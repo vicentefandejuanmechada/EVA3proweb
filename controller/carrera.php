1f426ce0b45f714f4aa36dd5dc8069ab070f3b7a
Pedro Garrido <pedro.munoz@tutelkan.com>
2022-07-03 21:56:24 -0400

    Fix delete button, add update button and form

--- controller/carrera.php
+++ controller/carrera.php
@@ -1,7 +1,28 @@
-<?php 
-    include '../model/carrera.php';
-    //recibe los datos del formulario
-    $carrera = $_POST['nombre'];
-    $result = insertarCarrera($carrera);
-    die($result);
-?>
+<?php
+include '../model/carrera.php';
+//recibe los datos del formulario
+if (isset($_POST['op'])) {
+    echo "anohere";
+    $op = $_POST['op'];
+    //inserta los datos en la base de datos
+    if ($op == 'insert') {
+        $nombre = $_POST['nombre'];
+        $result = insertarCarrera($nombre);
+        die($result);
+    } else if ($op == 'update') {
+        $id = $_POST['id'];
+        $nombre = $_POST['nombre'];
+        $result = updateCarrera($id, $nombre);
+        die($result);
+    }
+}
+
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+    if ($op == 'delete') {
+        $id = $_GET['id'];
+        $result = deleteCarrera($id);
+        echo "success";
+        die($result);
+    }
+}

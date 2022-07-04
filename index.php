634ea719a056ebf005dcdbf5e891ef31f2c45dde
Pedro Garrido <pedro.munoz@tutelkan.com>
2022-07-03 22:40:19 -0400

    better index

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-    <!-- DOCTYPE html-->
-    <html lang="en">
+<?php
+  include_once 'assets/include/menu.php';
+?>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Home</title>
-    </head>
+<!-- DOCTYPE html-->
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  </head>
 
-    <body>
-        <a href="views/alumno.php" class="btn btn-primary ">Alumno</a>
-        <a href="views/carrera.php" class="btn btn-primary ">Carrera</a>
-    </body>
-
-    </html>
+  <body>
+    <main>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1>Home</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <a href="views/alumno.php" class="btn btn-primary">Alumnos</a>
+            <a href="views/carrera.php" class="btn btn-primary">Carrera</a>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
